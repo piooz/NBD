72130b4f56e833ffcd76889c6f462487116f1858
wikslu <wiktoria.sluzewska@gmail.com>
2022-10-19 20:04:40 +0200

    movie show 2.0

--- src/main/java/model/Movie.java
+++ src/main/java/model/Movie.java
@@ -1,2 +1,83 @@
-package model;public class Movie {
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Valid
+@Access(AccessType.FIELD)
+public class Movie {
+    @Id
+    @GeneratedValue
+    private long Id;
+
+    @NotNull
+    private String title;
+    private int duration;
+    private String genre;
+    private String director;
+
+    public Movie(){
+
+    }
+
+    public Movie(long id, String title, int duration, String genre, String director) {
+        Id = id;
+        this.title = title;
+        this.duration = duration;
+        this.genre = genre;
+        this.director = director;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", duration=" + duration +
+                ", genre='" + genre + '\'' +
+                ", director='" + director + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
 }
