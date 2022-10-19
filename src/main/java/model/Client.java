07b0a91ac569459d9b92cf015ebdeb973c38f021
Piotr Zuchowski <236715@edu.p.lodz.pl>
2022-10-19 20:16:32 +0200

    add model classes

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -2,31 +2,52 @@ package model;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Valid
 @Access(AccessType.FIELD)
 public class Client {
-
     @Id
     @GeneratedValue
-    private long Id;
-    private String fistName;
+    private int Id;
+    private String firstName;
+    @Email
+    private String email;
+    public Client() { }
 
-    public long getId() {
+    public Client(String firstName, String email) {
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public int getId() {
         return Id;
     }
 
-    public void setId(long id) {
-        Id = id;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getFistName() {
-        return fistName;
+    @Override
+    public String toString() {
+        return "Client{" +
+                "Id=" + Id +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 
-    public void setFistName(String fistName) {
-        this.fistName = fistName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/main/java/model/Ticket.java
@@ -0,0 +1,86 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Valid
+@Access(AccessType.FIELD)
+public class Ticket {
+
+    public Ticket() {
+    }
+
+    public Ticket(int seatNumber, float price, Client client, Show show) {
+        this.seatNumber = seatNumber;
+        this.price = price;
+        this.client = client;
+        this.show = show;
+    }
+
+    @Id
+    @GeneratedValue
+    @Cascade({CascadeType.ALL})
+    private long id;
+    private int seatNumber;
+    private float price;
+    @ManyToOne
+    @JoinColumn(name = "client_fk")
+    private Client client;
+    @ManyToOne
+    @JoinColumn(name = "show_fk")
+    private Show show;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Show getShow() {
+        return show;
+    }
+
+    public void setShow(Show show) {
+        this.show = show;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id=" + id +
+                ", seatNumber=" + seatNumber +
+                ", price=" + price +
+                ", client=" + client +
+                ", show=" + show +
+                '}';
+    }
+}
