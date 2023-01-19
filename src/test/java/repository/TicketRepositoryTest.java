0744db0f6a04c106411894c2674dad37448f5e64
Piotr Zuchowski <236715@edu.p.lodz.pl>
2023-01-19 09:49:52 +0100

    add ticket tests

--- src/test/java/repository/TicketRepositoryTest.java
+++ src/test/java/repository/TicketRepositoryTest.java
@@ -2,20 +2,28 @@ package repository;
 
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.truncate.Truncate;
 import model.CinemaDDL;
 import model.Ticket;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TicketRepositoryTest {
 
     CqlSession session = CinemaDDL.initSession();
 
     TicketRepository ticketRepository = new TicketRepository(session);
+
+    void flushTable() {
+        Truncate statement = QueryBuilder.truncate("tickets");
+        SimpleStatement ss = statement.build();
+        session.execute(ss);
+    }
 
     @Test
     void get() {
@@ -41,13 +49,38 @@ class TicketRepositoryTest {
 
     @Test
     void remove() {
+
+        UUID uuid = new UUID(3,3);
+        UUID uuid2 = new UUID(3,4);
+        UUID uuid3 = new UUID(3,5);
+        Ticket tick = new Ticket(uuid,uuid2,uuid3, 30,2, Ticket.TicketType.Normal.name());
+
+        if(ticketRepository.get(uuid) == null) {
+            ticketRepository.add(tick);
+        }
+
+        assertDoesNotThrow( () -> ticketRepository.remove(tick));
+
+        assertNull(ticketRepository.get(uuid));
     }
 
     @Test
     void update() {
+
+        UUID uuid = new UUID(3,3);
+        UUID uuid2 = new UUID(3,4);
+        UUID uuid3 = new UUID(3,5);
+        Ticket tick = new Ticket(uuid,uuid2,uuid3, 30,2, Ticket.TicketType.Normal.name());
+        Ticket tick2 = new Ticket(uuid,uuid2,uuid3, 40,1, Ticket.TicketType.Reduced.name());
+
+        if(ticketRepository.get(uuid) == null) {
+            ticketRepository.add(tick);
+        }
+
+        assertDoesNotThrow(() -> ticketRepository.update(tick2));
+
+        assertEquals(tick2, ticketRepository.get(uuid));
+
     }
 
-    @Test
-    void find() {
-    }
 }
